ef8266ddc94d4351f8b0a7245ec25f26700648e2
ali.tekdemir <ali.tekdemir@gmail.com>
2025-01-20 20:31:10 +0000

    Geri bildirim butonu eklendi.

--- src/components/ZakirGullu.vue
+++ src/components/ZakirGullu.vue
@@ -1,42 +1,25 @@
 <!-- src/components/ZakirGullu.vue -->
 <template>
-  <div class="content">
-    <img 
-      src="../assets/rose.svg" 
-      class="rose" 
-      alt="Gül süslemesi"
-      loading="lazy"
-    />
-    <!-- src="../assets/zakir.png"  -->
-    <img 
-      src="../assets/zakir.png" 
-      class="zakir" 
-      alt="Zakir ikonu"
-      loading="lazy"
-    />
-    <img 
-      src="../assets/rose.svg" 
-      class="rose" 
-      alt="Gül süslemesi"
-      loading="lazy"
-    />
+  <div class="flex-container wrap">
+    <img src="../assets/rose.svg" class="rose" alt="Gül süslemesi" loading="lazy" />
+    <img src="../assets/zakir.png" class="zakir" alt="Zakir ikonu" loading="lazy" />
+    <img src="../assets/rose.svg" class="rose" alt="Gül süslemesi" loading="lazy" />
   </div>
-  <p class="acceptance-text">Allah kabul etsin kardeşim</p>
+  <p class="info-text">Allah kabul etsin kardeşim</p>
+
+  <div class="flex-container column feedback">
+    Uygulamayı geliştirmek için yardımınıza ihtiyacımız var. <br>
+    Görüş, öneri veya tespit ettiğiniz hataları bize iletirseniz çok memnun oluruz.
+    <button class="buton" onclick="window.location.href='https://tally.so/r/mJXPP4'">
+      <i class="material-symbols">edit_note</i>
+      Geri Bildirim Gönder
+    </button>
+  </div>
 </template>
 
 <style scoped>
-
-
-.content {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.2rem;
-}
-
 .rose {
   height: 2rem;
-  transition: filter 300ms;
 }
 
 .zakir {
@@ -48,9 +31,13 @@
   filter: drop-shadow(0 0 2em hwb(0 42% 0%));
 }
 
-.acceptance-text {
-  text-align: center;
+.feedback {
   margin: 1rem auto;
 }
 
+.buton {
+  font-size: 1.1rem;
+  /* font-weight: normal; */
+  align-items: center;
+}
 </style>
